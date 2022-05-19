3b2e4b98fe284f54420b5b9109b83a8b5a8ef43f
lindixxx <linda.pizzato03@htl-salzburg.ac.at>
2022-05-19 21:53:15 +0200

    created new checkpoints with materials
    Created new Prefabs for the Checkpoints with 3D Buttons, since the 2D UI-Buttons wouldn't work in VR.
    Added a trigger on the right hand, which detects the trigger with the button and enables them with the Script "Hand".
    Assigned them the right color with new materials in the same colors as eastereggs, but without their texture.

---
+++ Assets/MyGame/Scripts/Hand.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hand : MonoBehaviour
+{
+    Text numberText;
+    public Material disabled;
+    int number = 1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnButtonClick(other, "CountingUp", "Confirm", "number");
+        OnButtonClick(other, "CountingUp1", "Confirm1", "number1");
+        OnButtonClick(other, "CountingUp2", "Confirm2", "number2");
+        OnButtonClick(other, "CountingUp3", "Confirm3", "number3");
+    }
+
+    void OnButtonClick(Collider other, string countingUp, string confirm, string numberTag)
+    {
+        numberText = GameObject.FindGameObjectWithTag(numberTag).GetComponent<Text>();
+        numberText.text = number.ToString();
+
+        if (other.gameObject.tag == countingUp)
+        {
+            number++;
+            if (number > 9)
+            {
+                number = 1;
+            }
+        }
+
+        if (other.gameObject.tag == confirm)
+        {
+            GameObject.FindGameObjectWithTag(countingUp).GetComponent<Renderer>().material = disabled;
+            GameObject.FindGameObjectWithTag(countingUp).tag = "button";
+            GameObject.FindGameObjectWithTag(confirm).tag = "button";
+            Debug.Log("disabled");
+            number = 1;
+        }
+    }
+}
